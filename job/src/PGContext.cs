b9368ae13c593e5649288c6e92ac8befeff9f48b
weto <weto.jc@gmail.com>
2021-12-12 22:23:38 -0300

    add compose postgres, volume worker

---
+++ job/src/PGContext.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Microsoft;
+using job;
+
+/*namespace job
+{
+    public class PGContext : DbContext
+    {
+        public DbSet<ModelFila> Models { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres");
+    }
+}*/
